29e8a8d25cabb44143501ec85dfeac0e2db88a20
Shruti H. <167879533+shruti-harayan@users.noreply.github.com>
2026-01-31 20:25:54 +0530

    Add files via upload

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,5 +1,5 @@
 # backend/app/models/__init__.py
 from app.database import Base
-from app.models.user import User  
-from app.models.exam import Exam, Question, Student, Mark ,SubjectCatalog
+from app.models.user import User,PasswordReset
+from app.models.exam import Exam, Question, Student, Mark ,SubjectCatalog, ExamSection
 from app.models.programme import Programme
