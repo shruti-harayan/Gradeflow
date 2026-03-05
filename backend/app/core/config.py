c04a1f4c9a0f04b42fece5585890dd3720d30d0b
Shruti H. <167879533+shruti-harayan@users.noreply.github.com>
2026-03-05 16:55:54 +0530

    Refactor config.py to load environment variables

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,4 +1,9 @@
 import os
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
 
 RESEND_API_KEY = os.getenv("RESEND_API_KEY")
 EMAIL_FROM = os.getenv("EMAIL_FROM", "GradeFlow <onboarding@resend.dev>")
+APP_BASE_URL = getenv("APP_BASE_URL", "https://grade-frontend-vercel.vercel.app/")
