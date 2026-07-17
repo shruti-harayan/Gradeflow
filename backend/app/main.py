9fa250da5a4eb7cb1e63443bac5658cf88a8f9a4
Shruti H. <167879533+shruti-harayan@users.noreply.github.com>
2026-07-17 18:44:40 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -56,15 +56,14 @@ app = FastAPI(title="GradeFlow API")
 #         db.close()
 
 # 3. CORS SETTINGS
-origins = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "https://grade-frontend-vercel.vercel.app",
-],
-allow_origin_regex=r"https://.*\.app\.github\.dev",
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://grade-frontend-vercel.vercel.app",
+    ],
+    allow_origin_regex=r"https://.*\.app\.github\.dev",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
